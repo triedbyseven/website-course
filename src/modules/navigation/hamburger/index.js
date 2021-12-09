8e4c4c40ede37906db0319e24f1609b7d749960e
Michael Camacho <mikedev0431@gmail.com>
2021-12-09 11:32:06 -0800

    Added functionality to the hamburger menu button and overlay.

--- src/modules/navigation/hamburger/index.js
+++ src/modules/navigation/hamburger/index.js
@@ -1,19 +1,43 @@
 function loadHamburgerMenu() {
   const hamburgerMenu = document.getElementById('hamburger-menu');
   const menu = document.getElementById('menu');
+  const menuClose = document.getElementById('menu-close');
+  const navigationItems = document.getElementById('navigation-items');
   const overlay = document.getElementById('overlay');
 
   let isToggled = false;
 
+  let current = 0;
+
   menu.onclick = function () {
     isToggled = !isToggled;
 
-    console.log('isToggled is now set to: ', isToggled);
-
     overlay.classList.remove('overlay-hidden');
     overlay.classList.add('overlay-shown');
-    console.log(overlay.classList);
   };
+
+  menuClose.onclick = function () {
+    isToggled = !isToggled;
+
+    overlay.classList.remove('overlay-shown');
+    overlay.classList.add('overlay-hidden');
+  };
+
+
+  const navigationalItems = [...navigationItems.children];
+  
+  navigationalItems.forEach(navigationalItem => navigationalItem.classList.add('menu-item'));
+
+  const menuItemsElement = document.getElementsByClassName('menu-item');
+  const menuItems = [...menuItemsElement];
+
+  menuItems.forEach((menuItem, index) => menuItem.onclick = function () {
+    menuItems[current].classList.remove('active');
+
+    current = index;
+
+    menuItem.classList.add('active');
+  });
 
 };
 
