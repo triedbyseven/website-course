71aa3555a293257c69ee078e08f92bf35fd13a8b
Michael Camacho <mikedev0431@gmail.com>
2022-07-26 09:45:16 -0700

    Updated commit sync.

--- src/modules/api/credentials.js
+++ src/modules/api/credentials.js
@@ -1,5 +1,5 @@
 const apiUrl = 'https://api.github.com/graphql';
-const apiToken = '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';
+const apiToken = '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';
 
 export {
   apiUrl,
